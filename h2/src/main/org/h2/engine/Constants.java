1b1286dabb9a8ed56b34042016ce9dc0760f414c
dib.yang <dib.yang@gmail.com>
2026-08-18 18:45:32 +0800

    修复 MVStore 重叠块恢复与关闭竞态

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
      * Sequential version number. Even numbers are used for official releases,
      * odd numbers are used for development builds.
      */
-    public static final int BUILD_ID = 0;
+    public static final int BUILD_ID = 1;
 
     /**
      * Whether this is a snapshot version.
